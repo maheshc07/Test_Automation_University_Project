f2947fd3c1d391be8e89e41f4eb7d540f2812c3c
Mahesh Chandrashekar <mjmahi90@gmail.com>
2021-11-22 21:05:50 +0530

    Created test scripts for the automation framework

---
+++ src/test/java/sliders/SliderTests.java
@@ -0,0 +1,18 @@
+package sliders;
+
+import base.BaseTests;
+import lombok.var;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class SliderTests extends BaseTests {
+
+    @Test
+    public void testSlideToWholeNumber(){
+        String value = "4";
+        var sliderPage = homePage.clickHorizonalSlider();
+        sliderPage.setSliderValue(value);
+        assertEquals(sliderPage.getSliderValue(), value, "Slider value is incorrect");
+    }
+}
